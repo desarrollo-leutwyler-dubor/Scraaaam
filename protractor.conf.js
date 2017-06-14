843c6effba7bdbee04ec6020ab651dd64dad2c55
emanuel <emanuel.dubor@despegar.com>
2017-06-14 16:39:24 -0300

    Updated docker-compose file format to version 2

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,10 +1,11 @@
 exports.config = {
-  framework: 'mocha',
-  mochaOpts: {
-     timeout: 30000,
-  },
-  onPrepare: function () {
-    require("babel-register");
-  },
-  specs: ['test/e2e/dashboard.test.js']
+    seleniumAddress: process.env.WEBDRIVER || 'http://localhost:4444/wd/hub',
+    framework: 'mocha',
+    mochaOpts: {
+        timeout: 300000,
+    },
+    onPrepare: function () {
+        require("babel-register");
+    },
+    specs: ['test/e2e/dashboard.test.js']
 };
