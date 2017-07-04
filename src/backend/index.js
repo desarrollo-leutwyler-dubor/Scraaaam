186fd9fdb34d2b3763dfd2114144b90d9b6c1099
Ramshell <ramshellcinox@gmail.com>
2017-07-04 19:26:38 -0300

    add niffy feature

--- src/backend/index.js
+++ src/backend/index.js
@@ -4,5 +4,5 @@ import app from "./app"
 mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/projects')
 
 
-const port = 3001
+const port = process.env.APP_PORT || 3001
 app.listen(port, () => console.log(`Server running on port ${port}`))
